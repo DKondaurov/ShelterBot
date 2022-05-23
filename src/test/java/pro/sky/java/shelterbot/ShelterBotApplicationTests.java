53aeb92bdda49102edfb4816c86505b168d547d9
homework <9874@bk.ru>
2022-05-23 17:32:47 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/shelterbot/ShelterBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.shelterbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelterBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
